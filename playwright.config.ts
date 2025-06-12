269ceab51310db177db393be4dce26f8fd6e412b
AshfaqSourav <ashfaqahmed3339@gmail.com>
2025-06-12 20:49:47 +0600

    loading timeout increased

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,12 +8,12 @@ export default defineConfig({
   // reporter: 'html',
   use: {
     headless: false, 
-    navigationTimeout: 60000, // ⏳ Timeout for navigation actions like page.goto()
+    navigationTimeout: 120000, // ⏳ Timeout for navigation actions like page.goto()
     actionTimeout: 30000, 
     screenshot: 'only-on-failure',
     trace: 'off',
     viewport: null , // 👈 Let window size control the viewport
-
+    ignoreHTTPSErrors: true,
     launchOptions: {
       args: [
         //Cors Error Fix
@@ -21,7 +21,9 @@ export default defineConfig({
     // '--disable-features=IsolateOrigins,site-per-process',
     // '--allow-running-insecure-content',
     // '--disable-site-isolation-trials',
-    '--start-maximized'// 👈 Ask Chromium to open maximized
+    '--start-maximized',// 👈 Ask Chromium to open maximized
+    // '--ignore-certificate-errors',
+    // '--unsafely-treat-insecure-origin-as-secure=https://strapi-frontend-stage-2025-06-12-v1.nl-k8s-stage.srv.local'
       ]
     }
     
